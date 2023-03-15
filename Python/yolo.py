c4377158ddcc2a1be226ba94dd728b78a1a1f8a0
Dyxore <casperdamhus@live.dk>
2023-03-15 13:09:16 +0100

    Yolo working. Still needs to be a class

--- Python/yolo.py
+++ Python/yolo.py
@@ -117,7 +117,7 @@
 # np.random.seed(42)
 # colors_rnd = np.random.randint(0, 255, size=(len(classes_names), 3), dtype='uint8')
 
-# net_yolo = cv2.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
+# net_yolo = cv2.dnn.readNetFromDarknet('yolov3.cfg', '/home/pi/yolov3.weights')
 # net_yolo.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 # net_yolo.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
@@ -171,57 +171,140 @@
 
 
 
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+# from picamera.array import PiRGBArray
+# from picamera import PiCamera
+# import cv2
+# import numpy as np
+
+
+# def imageFlip(image):
+#     image = cv2.flip(image, 0)
+#     image = cv2.flip(image, 1)
+#     return image
+
+
+# camera = PiCamera()
+# rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# classes_names = open('coco.names').read().strip().split('\n')
+# np.random.seed(42)
+# colors_rnd = np.random.randint(0, 255, size=(len(classes_names), 3), dtype='uint8')
+
+# net_yolo = cv2.dnn.readNetFromDarknet('yolov3.cfg', '/home/yolov3.weights')
+# net_yolo.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+# net_yolo.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+
+# ln = net_yolo.getLayerNames()
+# ln = [ln[i - 1] for i in net_yolo.getUnconnectedOutLayers()]
+
+
+# for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#     # grab the raw NumPy array representing the image, then initialize the timestamp
+#     # and occupied/unoccupied text
+#     image = frame.array
+#     # show the frame
+#     image = imageFlip(image)
+#     cv2.imshow("Frame", image)
+#     key = cv2.waitKey(1) & 0xFF
+#     # clear the stream in preparation for the next frame
+#     rawCapture.truncate(0)
+
+#     blob_img = cv2.dnn.blobFromImage(image, 1/255.0, (640, 480), swapRB=True, crop=False)
+#     r_blob = blob_img[0, 0, :, :]
+
+#     cv2.imshow('blob', r_blob)
+#     text = f'Blob shape={blob_img.shape}'
+
+#     net_yolo.setInput(blob_img)
+#     outputs = net_yolo.forward(ln)
+
+#     boxes = []
+#     confidences = []
+#     classIDs = []
+#     h, w = image.shape[:2]
+
+#     for output in outputs:
+#         for detection in output:
+#             scores_yolo = detection[5:]
+#             classID = np.argmax(scores_yolo)
+#             confidence = scores_yolo[classID]
+#             if confidence > 0.5:
+#                 box_rect = detection[:4] * np.array([w, h, w, h])
+#                 (centerX, centerY, width, height) = box_rect.astype("int")
+#                 x_c = int(centerX - (width / 2))
+#                 y_c = int(centerY - (height / 2))
+#                 box_rect = [x_c, y_c, int(width), int(height)]
+#                 boxes.append(box_rect)
+#                 confidences.append(float(confidence))
+#                 classIDs.append(classID)
+
+#     indices_yolo = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+#     if len(indices_yolo) > 0:
+#         for i in indices_yolo.flatten():
+#             (x, y) = (boxes[i][0], boxes[i][1])
+#             (w, h) = (boxes[i][2], boxes[i][3])
+#             color = [int(c) for c in colors_rnd[classIDs[i]]]
+#             cv2.rectangle(image, (x, y), (x + w, y + h), color, 3)
+#             text = "{}: {:.4f}".format(classes_names[classIDs[i]], confidences[i])
+#             cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+
+#     cv2.imshow('window', image)
+
+#     # if the `q` key was pressed, break from the loop
+#     if key == ord("q"):
+#         break
+    
+#     cv2.waitKey(1)
+
+# cv2.destroyAllWindows()
+
+
+
+
+
 import cv2
 import numpy as np
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 
 def imageFlip(image):
-    image = cv2.flip(image, 0)
-    image = cv2.flip(image, 1)
-    return image
+        image = cv2.flip(image, 0)
+        image = cv2.flip(image, 1)
+        return image
 
 
 camera = PiCamera()
-rawCapture = PiRGBArray(camera, size=(640, 480))
+camera.resolution = (160, 120)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=camera.resolution)
 
 classes_names = open('coco.names').read().strip().split('\n')
 np.random.seed(42)
 colors_rnd = np.random.randint(0, 255, size=(len(classes_names), 3), dtype='uint8')
 
-net_yolo = cv2.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
+net_yolo = cv2.dnn.readNetFromDarknet('yolov3.cfg', '/home/pi/yolov3.weights')
 net_yolo.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net_yolo.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 ln = net_yolo.getLayerNames()
 ln = [ln[i - 1] for i in net_yolo.getUnconnectedOutLayers()]
 
-
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-    # show the frame
-    image = imageFlip(image)
-    cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+    img_src = frame.array
 
-    blob_img = cv2.dnn.blobFromImage(image, 1/255.0, (640, 480), swapRB=True, crop=False)
+    img_src = imageFlip(img_src)
+    
+    blob_img = cv2.dnn.blobFromImage(img_src, 1/255.0, (128, 128), swapRB=True, crop=False)
     r_blob = blob_img[0, 0, :, :]
-
-    cv2.imshow('blob', r_blob)
-    text = f'Blob shape={blob_img.shape}'
-
+    
     net_yolo.setInput(blob_img)
     outputs = net_yolo.forward(ln)
 
     boxes = []
     confidences = []
     classIDs = []
-    h, w = image.shape[:2]
+    h, w = img_src.shape[:2]
 
     for output in outputs:
         for detection in output:
@@ -237,6 +320,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 boxes.append(box_rect)
                 confidences.append(float(confidence))
                 classIDs.append(classID)
+                print("x_c: ", x_c)
 
     indices_yolo = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
     if len(indices_yolo) > 0:
@@ -244,16 +328,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             (x, y) = (boxes[i][0], boxes[i][1])
             (w, h) = (boxes[i][2], boxes[i][3])
             color = [int(c) for c in colors_rnd[classIDs[i]]]
-            cv2.rectangle(image, (x, y), (x + w, y + h), color, 3)
+            cv2.rectangle(img_src, (x, y), (x + w, y + h), color, 3)
             text = "{}: {:.4f}".format(classes_names[classIDs[i]], confidences[i])
-            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
+            cv2.putText(img_src, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
 
-    cv2.imshow('window', image)
-
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+    cv2.imshow('window', img_src)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
-    cv2.waitKey(1)
+
+    rawCapture.truncate(0)
 
 cv2.destroyAllWindows()
